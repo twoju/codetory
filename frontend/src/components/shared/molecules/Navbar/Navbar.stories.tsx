844c7b3a450b7d0ffcce2eed8de14d23eb83d568
조원주 <dnjswn9178@gmail.com>
2024-02-25 01:38:02 +0900

    feat: navbar 파일 생성

---
+++ frontend/src/components/shared/molecules/Navbar/Navbar.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Navbar from '.';
+
+const meta: Meta<typeof Navbar> = {
+  title: 'Molecule/Navbar',
+  component: Navbar,
+  parameters: {
+    layout: 'fullscreen',
+    componentSubtitle: '헤더 문서입니다.',
+    docs: {
+      description: {
+        component: `Navbar 에 대한 내용을 설명하기`,
+      },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Navbar>;
+
+export const Default: Story = {
+  args: {
+    page: 'default',
+    title: '로드맵',
+  },
+};
