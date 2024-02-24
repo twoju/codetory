30dafdf98890fdd0793de1e3d125f2a22449f508
조원주 <dnjswn9178@gmail.com>
2024-02-25 01:38:50 +0900

    feat: svg 파일 테스트를 위한 컴포넌트 추가

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,10 @@
 import Button from '../components/shared/atoms/Button';
+import { ReactComponent as LineHome } from '../assets/svgs/lineHome.svg';
 
 function Home() {
   return (
     <>
+      <LineHome/>
       <Button text="입장하기" themes="default" />
     </>
   );
