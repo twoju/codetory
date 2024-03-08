bab359247ba0e067231405ead62cc054f1ace42e
조원주 <dnjswn9178@gmail.com>
2024-03-08 15:58:21 +0900

    feat: axios 통신 테스트

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,13 @@
 import Button from '../components/shared/atoms/Button';
 import LineHome from '../assets/svgs/lineHome.svg';
 import FilledHome from '../assets/svgs/filledHome.svg?react';
+import { useEffect } from 'react';
+import { adminApi } from '../apis';
 
 function Home() {
+  useEffect(() => {
+    adminApi.homeTest().then((res) => console.log(res));
+  }, []);
   return (
     <>
       <img src={LineHome} alt="svg" />
